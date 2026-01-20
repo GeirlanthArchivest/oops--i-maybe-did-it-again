51cad8de9a398903c966d18a56694600226f6e38
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2026-01-20 16:05:52 +0000

    bullets and missiles now die on hitting ground

--- oops!-i-maybe-did-it-again/Scripts/Missile.gd
+++ oops!-i-maybe-did-it-again/Scripts/Missile.gd
@@ -6,3 +6,7 @@ var speed = 100
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y += 1 * speed * delta
+
+func _on_hitbox_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("Floor"):
+		queue_free()

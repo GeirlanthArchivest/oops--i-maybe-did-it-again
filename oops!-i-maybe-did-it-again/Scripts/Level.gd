f4047c2efe1e04f8d0461d1555a6b74319c1c16a
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2026-01-20 15:53:31 +0000

    missiles fall regardless of player movement

--- oops!-i-maybe-did-it-again/Scripts/Level.gd
+++ oops!-i-maybe-did-it-again/Scripts/Level.gd
@@ -3,7 +3,6 @@ extends Node2D
 var building = preload("res://Scenes/building.tscn")
 var missile_path = preload("res://Scenes/missile.tscn")
 @onready var camera = $Camera2D
-@onready var masterSP = $Camera2D/Spawnpointmaster
 
 func _ready():
 	Global.node_creation_parent = self
@@ -13,15 +12,18 @@ func _exit_tree():
 
 func _process(_delta: float) -> void:
 	camera.position = Global.player.global_position
-	masterSP.position = Global.player.global_position
 	
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
 
-#func _on_missile_timer_timeout() -> void:
-	#var missile = missile_path.instantiate()
-	#add_child(missile)
-	#missile.position = $Spawn.position
+func _on_missile_timer_timeout() -> void:
+	var missile = missile_path.instantiate()
+	add_child(missile)
+	
+	var area = $Camera2D/SpawnArea
+	var random_offset = Vector2(randf_range(0, area.size.x), randf_range(0, area.size.y))
+	missile.global_position = area.global_position + random_offset
+	
 	#
 	#var nodes = get_tree().get_nodes_in_group("spawn")
 	#var node = nodes[randi() % nodes.size()]

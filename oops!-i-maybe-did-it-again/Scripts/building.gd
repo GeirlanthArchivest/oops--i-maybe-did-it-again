1bcd0bd29780a22fac118ca5275eecbc6c0fc24d
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2026-01-21 11:43:26 +0000

    buildings are textured randomly at game start

--- oops!-i-maybe-did-it-again/Scripts/building.gd
+++ oops!-i-maybe-did-it-again/Scripts/building.gd
@@ -6,10 +6,15 @@ var max_health = 20
 var minimum_health = 5
 var building_health
 
+var building_texture1 = preload("res://Assets/Building 1.png")
+var building_texture2 = preload("res://Assets/Building 2.png")
+var building_texture3 = preload("res://Assets/Building 3.png")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	building_health = randf_range(minimum_health, max_health)
 	health = building_health
+	building_asset()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float):
@@ -25,3 +30,16 @@ func _on_hitbox_area_entered(area: Area2D):
 			Global.numOfBuildings.remove_at(index)
 			print(Global.numOfBuildings)
 			queue_free()
+
+func building_asset():
+	var building_asset_pick = randi_range(-1,4)
+	print(building_asset_pick)
+	if building_asset_pick == 0:
+		texture = building_texture1
+		offset.y = -300
+	elif building_asset_pick == 1:
+		texture = building_texture2
+		offset.y = -300
+	elif building_asset_pick == 2:
+		texture = building_texture3
+		offset.y = -182

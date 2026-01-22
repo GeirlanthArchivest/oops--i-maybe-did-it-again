1b69b250d11d883ee83fa10bd00eeb97ecb2dced
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2026-01-22 11:06:26 +0000

    can restart or go to main menu via buttons on end screens

--- oops!-i-maybe-did-it-again/Scripts/win_screen.gd
+++ oops!-i-maybe-did-it-again/Scripts/win_screen.gd
@@ -1,10 +1,27 @@
 extends Control
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if Global.win:
 		$"Win Screen".visible = true
 	else:
 		$"Lose Screen".visible = true
+
+func _on_lose_home_button_pressed() -> void:
+	home()
+
+func _on_lose_restart_button_pressed() -> void:
+	restart()
+
+func _on_win_home_button_pressed() -> void:
+	home()
+
+func _on_win_restart_button_pressed() -> void:
+	restart()
+
+func restart():
+	get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
+
+func home():
+	get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
